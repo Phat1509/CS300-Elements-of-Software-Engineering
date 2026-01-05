66bde85c44d0e6c023610599a9e4306ca0524380
ThanhPhat1509 <lethanhphat2005xt@gmail.com>
2026-01-05 20:32:22 +0700

    delete server folder

--- ShoesStore/server/src/types/api.ts
+++
@@ -1,6 +0,0 @@
-import { t, TSchema } from "elysia";
-
-export const UnsuccessfulAPIResponse = t.Object({
-    error: t.String(),
-});
-export type UnsuccessfulAPIResponse = typeof UnsuccessfulAPIResponse.static;
